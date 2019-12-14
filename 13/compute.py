c070f834322f7297d20031f2ba9892a9e94985e1
Francesco Di Baldassarre <f.dibaldassarre@Raphael>
2019-12-14 21:21:40 +0100

    Code cleanup

--- 13/compute.py
+++ 13/compute.py
@@ -4,7 +4,6 @@ import os
 import sys
 
 import numpy as np
-from time import sleep
 
 from src.interpreter import OptcodeInterpreter
 from src.utils import get_input
@@ -15,7 +14,7 @@ TILE_BLOCK = 2
 TILE_PADDLE = 3
 TILE_BALL = 4
 
-RENDER_MAP = {
+RENDER_MAP_EMOJI = {
     TILE_EMPTY: ' ',
     TILE_WALL: '🎄',
     TILE_BLOCK: '❄️',
@@ -23,9 +22,13 @@ RENDER_MAP = {
     TILE_BALL: '🌟',
 }
 
-MOVE_LEFT = -1
-MOVE_RIGHT = 1
-WAIT = 0
+RENDER_MAP = {
+    TILE_EMPTY: ' ',
+    TILE_WALL: 'X',
+    TILE_BLOCK: '#',
+    TILE_PADDLE: '=',
+    TILE_BALL: 'o',
+}
 
 
 def get_image_shape(table):
@@ -41,128 +44,125 @@ def get_image_shape(table):
 
 class Player:
 
-    def __init__(self, grid):
-        self.grid = grid
+    def __init__(self, arcade_screen):
+        self.arcade_screen = arcade_screen
 
     def act(self):
-        ball_x, _ = self.get_element_position(TILE_BALL)
-        paddle_x, _ = self.get_element_position(TILE_PADDLE)
-        if paddle_x < ball_x:
-            return MOVE_RIGHT
-        elif ball_x < paddle_x:
-            return MOVE_LEFT
-        else:
-            return WAIT
-
-    def get_element_position(self, target):
-        for pos in self.grid:
-            if self.grid[pos] == target:
-                return pos
+        ball_x, _ = self.arcade_screen.get_ball_position()
+        paddle_x, _ = self.arcade_screen.get_paddle_position()
+        return np.sign(ball_x - paddle_x)
 
 
-class Grid:
+class ArcadeScreen:
 
     def __init__(self):
-        self.grid = {}
+        self.screen = {}
         self.segment_display = 0
+        self._shape = None
+        self._position_ball = None
+        self._position_paddle = None
 
     def draw(self, position, tile):
         x, y = position
         if x == -1 and y == 0:
             self.segment_display = tile
         else:
-            self.grid[position] = tile
+            self.screen[position] = tile
+            if tile == TILE_PADDLE:
+                self._position_paddle = position
+            elif tile == TILE_BALL:
+                self._position_ball = position
+
+    def get_ball_position(self):
+        return self._position_ball
+
+    def get_paddle_position(self):
+        return self._position_paddle
 
     def get_screen(self):
-        return self.grid
+        return self.screen
 
     def get_score(self):
         return self.segment_display
 
+    def _get_shape(self):
+        if self._shape is None:
+            _, self._shape = get_image_shape(self.screen)
+        return self._shape
+
     def render(self):
-        delta, size = get_image_shape(self.grid)
-        image = np.zeros(shape=size, dtype=np.uint8)
-        dy, dx = delta
-        for y, x in self.grid:
-            image[y + dy, x + dx] = self.grid[y, x]
         os.system("clear")  # Linux only
-        print("Score:", self.segment_display)
-        max_y, max_x = size
+        print("\tScore:", self.segment_display)
+        max_y, max_x = self._get_shape()
         for x in range(max_x):
             for y in range(max_y):
-                sys.stdout.write(RENDER_MAP[self.grid[y, x]])
+                sys.stdout.write(RENDER_MAP[self.screen[y, x]])
             sys.stdout.write("\n")
         sys.stdout.flush()
 
 
-def compute():
+def read_input():
     # Run the program
     with open("input", "r") as hand:
         optcode_raw = hand.read()
     optcode = list(map(lambda el: int(el), optcode_raw.split(",")))
+    return optcode
 
+
+def draw_instructions(interpreter, arcade_screen):
+    try:
+        x = interpreter.output.get(timeout=1)
+    except Exception:
+        pass
+    y = interpreter.output.get()
+    tile = interpreter.output.get()
+    arcade_screen.draw((x, y), tile)
+
+
+def compute():
+    optcode = read_input()
     interpreter = OptcodeInterpreter(optcode, quiet_mode=True)
     thread = interpreter.run_async()
-    grid = Grid()
+    arcade_screen = ArcadeScreen()
     while thread.is_alive():
-        try:
-            x = interpreter.output.get(timeout=2)
-        except Exception:
-            continue
-        y = interpreter.output.get()
-        tile = interpreter.output.get()
-        grid.draw((x, y), tile)
-    # Count blocks
-    screen = grid.get_screen()
-    count = 0
-    for el in screen:
-        if screen[el] == TILE_BLOCK:
-            count += 1
-    return count
-
-
-def draw_input(interpreter, grid):
+        draw_instructions(interpreter, arcade_screen)
     while not interpreter.output.empty():
-        try:
-            x = interpreter.output.get(timeout=2)
-        except Exception:
-            pass
-        y = interpreter.output.get()
-        tile = interpreter.output.get()
-        grid.draw((x, y), tile)
+        draw_instructions(interpreter, arcade_screen)
+    screen = arcade_screen.get_screen()
+    return len(list(filter(lambda pos: screen[pos] == TILE_BLOCK, screen)))
 
 
 def compute_play(interactive=True):
-    # Run the program
-    with open("input", "r") as hand:
-        optcode_raw = hand.read()
-    optcode = list(map(lambda el: int(el), optcode_raw.split(",")))
-
+    if interactive:
+        print("Press A to move left, D to move right")
+        print("Press ENTER to start")
+        input()
+    optcode = read_input()
     interpreter = OptcodeInterpreter(optcode, quiet_mode=True)
     interpreter.memory[0] = 2
-    grid = Grid()
-    player = Player(grid.get_screen())
+    arcade_screen = ArcadeScreen()
+    player = Player(arcade_screen)
 
     def on_input_requested():
-        draw_input(interpreter, grid)
+        while not interpreter.output.empty():
+            draw_instructions(interpreter, arcade_screen)
         if interactive:
-            grid.render()
+            arcade_screen.render()
             int_input = get_input()
         else:
             int_input = player.act()
         interpreter.input.put(int_input)
 
     interpreter.set_input_request_listener(on_input_requested)
-    thread = interpreter.run_async()
-    while thread.is_alive():
-        sleep(0.5)
-    draw_input(interpreter, grid)
+    interpreter.run()
+    while not interpreter.output.empty():
+        draw_instructions(interpreter, arcade_screen)
     if interactive:
-        grid.render()
-    return grid.get_score()
+        arcade_screen.render()
+    return arcade_screen.get_score()
 
 
 tiles = compute()
-print("Tiles", tiles)
-score = compute_play(interactive=True)
+print("Block tiles:", tiles)
+score = compute_play(interactive=False)
 print("Final score:", score)
